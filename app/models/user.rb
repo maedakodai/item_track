a3cc93a4f7417d475a14b7718f50717805f176ea
kodai maeda <isleep4070@icloud.com>
2024-02-21 15:09:57 +0900

    add:dependent: :destroy追加 #7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  has_many :items, dependent: :destroy
 end
