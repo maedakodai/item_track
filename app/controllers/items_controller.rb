1743f821fd7d053aea78f8f6f07bcee26dcf9488
kodai maeda <isleep4070@icloud.com>
2024-03-01 19:34:40 +0900

    add:ステータス自動更新とフラッシュメッセージ追加 #40

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  before_action :check_items_status
   def index
     @items = current_user.items.order(:id)
   end
@@ -24,16 +24,32 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = current_user.items.find(params[:id])
-    item.update!(item_params)
-    redirect_to items_path
+    @item = current_user.items.find(params[:id])
+    if @item.update(item_params)
+      @item.to_using# アイテムのステータスをチェックして必要に応じて更新
+      flash[:success] = "#{@item.name}が使用中に更新されました。"
+      redirect_to items_path
+    else
+      render :edit
+    end
   end
+
 
   def destroy
     item = current_user.items.find(params[:id])
     item.destroy
-    redirect_to items_path, notice: "アイテム#{item.name}を削除しました"
+    flash[:success] = "#{item.name}を削除しました。"
+    redirect_to items_path 
   end
+
+  def complete
+    @item = current_user.items.find(params[:id])
+    @item.completed!
+    flash[:success] = "#{@item.name}交換完了に更新されました。"
+    redirect_to items_path 
+  end
+
+
 
   private
 
@@ -41,4 +57,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :reminder_days_before_end, :start_at, :end_at)
   end
 
+  def check_items_status
+    current_user.items.each do |item|
+      item.to_recommended
+    end
+  end
 end
