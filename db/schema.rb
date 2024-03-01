fc77f49536eb2852ffb9ae6a17d9133259e8daca
kodai maeda <isleep4070@icloud.com>
2024-03-01 19:14:34 +0900

    add:itemsテーブルにstatusカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_070339) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_044254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_070339) do
     t.datetime "updated_at", null: false
     t.date "start_at", null: false
     t.date "end_at"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
